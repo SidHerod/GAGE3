b9ae12c5f97cc7f0015a45f199f96652ac07df71
SidHerod <simonherod22@Hotmail.co.uk>
2025-06-02 13:05:52 +1000

    Update AgeGuessingScreen.tsx

--- components/AgeGuessingScreen.tsx
+++ components/AgeGuessingScreen.tsx
@@ -1,3 +1,4 @@
+import { db } from '../firebase';
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useProfile } from '../contexts/ProfileContext';
