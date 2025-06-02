91c5d8ffc8bd641cadc2aff4876d0d6afe60b817
SidHerod <simonherod22@Hotmail.co.uk>
2025-06-02 14:13:26 +1000

    Update StatisticsScreen.tsx

--- components/StatisticsScreen.tsx
+++ components/StatisticsScreen.tsx
@@ -1,4 +1,4 @@
-import { db } from '../firebase';
+import { db } from './firebase';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useProfile } from '../contexts/ProfileContext';
